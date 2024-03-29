4eeeba5eeffaa7f7e919a7cc3183f2d350b92fd0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-03-29 11:00:43 -0300

    Cap 12: gateway para unificar as rotas

---
+++ gateway-api/src/test/java/com/marcos/silva/rodrigues/gatewayapi/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.silva.rodrigues.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
